62fcf4eefc698c86904ce9e9c83d9a133fb80302
Shalin Patel <shalin.kp.patel@gmail.com>
2021-02-03 17:27:00 -0500

    added subgraph

--- torch_geometric/nn/models/pg_explainer.py
+++ torch_geometric/nn/models/pg_explainer.py
@@ -32,4 +32,25 @@ class PGExplainer(torch.nn.Module):
                 k += 1
         return k
 
-    
+    def __flow__(self):
+        for module in self.model.modules():
+            if isinstance(module, MessagePassing):
+                return module.flow
+        return 'source_to_target'
+
+    def __subgraph__(self, node_idx, x, edge_index, **kwargs):
+        num_nodes, num_edges = x.size(0), edge_index.size(1)
+
+        subset, edge_index, mapping, edge_mask = k_hop_subgraph(
+            node_idx, self.num_hops, edge_index, relabel_nodes=True,
+            num_nodes=num_nodes, flow=self.__flow__())
+
+        x = x[subset]
+        for key, item in kwargs.items():
+            if torch.is_tensor(item) and item.size(0) == num_nodes:
+                item = item[subset]
+            elif torch.is_tensor(item) and item.size(0) == num_edges:
+                item = item[edge_mask]
+            kwargs[key] = item
+
+        return x, edge_index, mapping, edge_mask, kwargs
