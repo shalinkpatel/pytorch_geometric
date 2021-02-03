3d7d514d2ef6a1345d6209e0a9d82cc9d409d12d
Shalin Patel <shalin.kp.patel@gmail.com>
2021-02-03 17:18:33 -0500

    finished init and num_hops

--- torch_geometric/nn/models/pg_explainer.py
+++ torch_geometric/nn/models/pg_explainer.py
@@ -10,4 +10,26 @@ from torch_geometric.data import Data
 from torch_geometric.utils import k_hop_subgraph, to_networkx
 
 class PGExplainer(torch.nn.Module):
-    pass
+    def __init__(self, embed_model, output_model, epcohs: int = 100, 
+                lr: float = 0.01, num_hops: Optional[int] = None, 
+                log: bool = True):
+        super(PGExplainer, self).__init__()
+        self.embed_model = embed_model
+        self.output_model = output_model
+        self.epochs = epcohs
+        self.lr = lr
+        self.__num_hops__ = num_hops
+        self.log = log
+
+    @property
+    def num_hops(self):
+        if self.__num_hops__ is not None:
+            return self.__num_hops__
+
+        k = 0
+        for module in self.model.modules():
+            if isinstance(module, MessagePassing):
+                k += 1
+        return k
+
+    
